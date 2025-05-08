043b4cb75a6a405fa9f8c10d32354d4f378a1996
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-05-08 16:48:43 +0100

    adding model output

--- question3.py
+++ question3.py
@@ -4,6 +4,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.metrics import accuracy_score
 
 from data_loader import loadDataH5
 
@@ -43,15 +45,54 @@ def resnet50_model(epochs_count, trainX, trainY, valX, valY):
 
     print(f'ResNet50 Model: {model.summary()}')
 
-    resnet50_model_result = model.fit(trainX, trainY,
-                                              epochs=epochs_count,
-                                              batch_size=32,
-                                              validation_data=(valX, valY))
+
+
+    phaseA_result = model.fit(trainX,
+                                      trainY,
+                                      epochs=epochs_count,
+                                      batch_size=32,
+                                      validation_data=(valX, valY))
+    print(f'ResNet50 phase A history: {phaseA_result.history}')
+
+    base_model.trainable = True
+    trainable_flag = False
+
+    for layer in base_model.layers:
+        if layer.name == 'conv5_block3_1_conv':
+            trainable_flag = True
+        layer.trainable = trainable_flag
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-4),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    print(f'ResNet50 Model: {model.summary()}')
+
+    checkpoint = ModelCheckpoint(
+        filepath=f"{MODEL_PATH}/resnet50.h5",
+        monitor='val_accuracy',
+        save_best_only=True,
+        save_weights_only=False,
+        mode='max',
+        verbose=1
+    )
+
+    resnet50_model_result = model.fit(trainX,
+                                      trainY,
+                                      epochs=epochs_count,
+                                      batch_size=32,
+                                      validation_data=(valX, valY),
+                                      callbacks=[checkpoint])
 
     print(f'ResNet50 history: {resnet50_model_result.history}')
-    model_file_path = f"{MODEL_PATH}/resnet50.h5"
-    model.save(model_file_path)
-    return model
+
+    resnet50_preds = np.argmax(model.predict(valX), axis=1)
+    resnet50_acc = accuracy_score(valY, resnet50_preds)
+    print(f'resnet50_acc: {resnet50_acc}')
+
+    return resnet50_acc
 
 def vgg16_model(epochs_count, trainX, trainY, valX, valY):
     base_model = VGG16(weights="imagenet", include_top=False, input_shape=(64, 64, 3))
@@ -85,33 +126,69 @@ def vgg16_model(epochs_count, trainX, trainY, valX, valY):
 
     print(f'VGG16 Model: {model.summary()}')
 
-    vgg16_model_result = model.fit(trainX, trainY,
-                             epochs=epochs_count,
-                             batch_size=32,
-                             validation_data=(valX, valY))
+    phaseA_result = model.fit(trainX,
+                              trainY,
+                              epochs=epochs_count,
+                              batch_size=32,
+                              validation_data=(valX, valY))
+    print(f'VGG16 phase A history: {phaseA_result.history}')
+
+    base_model.trainable = True
+    trainable_flag = False
+
+    for layer in base_model.layers:
+        if layer.name == 'block4_conv1':
+            trainable_flag = True
+        layer.trainable = trainable_flag
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-4),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    checkpoint = ModelCheckpoint(
+        filepath=f"{MODEL_PATH}/vgg16.h5",
+        monitor='val_accuracy',
+        save_best_only=True,
+        save_weights_only=False,
+        mode='max',
+        verbose=1
+    )
+
+    vgg16_model_result = model.fit(trainX,
+                              trainY,
+                              epochs=epochs_count,
+                              batch_size=32,
+                              validation_data=(valX, valY),
+                              callbacks=[checkpoint])
 
     print(f'VGG16 history: {vgg16_model_result.history}')
-    model_file_path = f"{MODEL_PATH}/vgg16.h5"
-    model.save(model_file_path)
-    return model
 
-def ensemble_prediction(image):
-    single_input = np.expand_dims(image, axis=0)
-    print(f'single_input shape: {single_input.shape}')
+    vgg16_preds = np.argmax(model.predict(valX), axis=1)
+    vgg16_acc = accuracy_score(valY, vgg16_preds)
+    print(f'vgg16_acc: {vgg16_acc}')
 
+    return vgg16_acc
+
+def soft_voting_ensemble(valX, valY):
     vgg16 = load_model(f"{MODEL_PATH}/vgg16.h5")
-    vgg16_prediction = vgg16.predict(single_input)
+    vgg16_prediction = vgg16.predict(valX)
     print(f'vgg16 prediction values: {vgg16_prediction}')
     print(f'vgg16 prediction type: {type(vgg16_prediction)}')
 
     resnet50 = load_model(f"{MODEL_PATH}/resnet50.h5")
-    resnet50_prediction = resnet50.predict(single_input)
+    resnet50_prediction = resnet50.predict(valX)
     print(f'resnet50 prediction values: {resnet50_prediction}')
     print(f'resnet50 prediction type: {type(resnet50_prediction)}')
 
-    final_prediction = vgg16_prediction + resnet50_prediction
-    print(f'final prediction values: {final_prediction}')
-    print(f'Final prediction class: {np.argmax(final_prediction)}')
+    ensemble_preds = (vgg16_prediction + resnet50_prediction) / 2
+    print(f'final prediction values: {ensemble_preds}')
+
+    ensemble_acc = accuracy_score(valY, ensemble_preds)
+    print(f'ensemble_acc: {ensemble_acc}')
+
+    return ensemble_acc
 
 if __name__ == "__main__":
     print(tf.config.list_physical_devices('GPU'))
@@ -121,7 +198,14 @@ if __name__ == "__main__":
     print(tf.executing_eagerly())
     if len(physical_devices) > 0:
         trainX, trainY, valX, valY = loadDataH5()
-        resnet50 = resnet50_model(2, trainX, trainY, valX, valY)
-        vgg16 = vgg16_model(2, trainX, trainY, valX, valY)
-        ensemble_prediction(valX[0])
+        resnet50_acc = resnet50_model(2, trainX, trainY, valX, valY)
+        vgg16_acc = vgg16_model(2, trainX, trainY, valX, valY)
+        soft_voting_ensemble_acc = soft_voting_ensemble(valX, valY)
+
+        print(f"VGG16 Accuracy:     {vgg16_acc:.4f}")
+        print(f"ResNet50 Accuracy:  {resnet50_acc:.4f}")
+        print(f"Ensemble(Soft Voting) Accuracy:  {soft_voting_ensemble_acc:.4f}")
+
+
+
 
